f9691371766c5d7b555d6702b89313a91227fdde
Sérgio Alves Lima Jardim <sergio@plasmadesign.com.br>
2016-03-29 18:55:38 -0300

    Header is now fixed on mobile

--- js/main.js
+++ js/main.js
@@ -10,12 +10,67 @@ jQuery(document).ready(function(){
 		searchForm = $('.cd-search'),
 		accountInfo = $('.account');
 
+	function checkMQ() {
+		//check if mobile or desktop device
+		return window.getComputedStyle(document.querySelector('.cd-main-content'), '::before').getPropertyValue('content').replace(/'/g, "").replace(/"/g, "");
+	}
+
+	function moveNavigation(){
+  		var mq = checkMQ();
+        
+        if ( mq === 'mobile' && topNavigation.parents('.cd-side-nav').length === 0 ) {
+        	detachElements();
+			topNavigation.appendTo(sidebar);
+			searchForm.removeClass('is-hidden').prependTo(sidebar);
+		} else if ( ( mq === 'tablet' || mq === 'desktop') &&  topNavigation.parents('.cd-side-nav').length > 0 ) {
+			detachElements();
+			searchForm.insertAfter(header.find('.cd-logo'));
+			topNavigation.appendTo(header.find('.cd-nav'));
+		}
+		checkSelected(mq);
+		resizing = false;
+	}
+
+	function detachElements() {
+		topNavigation.detach();
+		searchForm.detach();
+	}
+
+	function checkSelected(mq) {
+		//on desktop, remove selected class from items selected on mobile/tablet version
+		if( mq === 'desktop' ) {
+			$('.has-children.selected').removeClass('selected');
+		}
+	}
+
+	function checkScrollbarPosition() {
+		var mq = checkMQ();
+		
+		if( mq !== 'mobile' ) {
+			var sidebarHeight = sidebar.outerHeight(),
+				windowHeight = $(window).height(),
+				mainContentHeight = mainContent.outerHeight(),
+				scrollTop = $(window).scrollTop();
+
+			if ( ( scrollTop + windowHeight > sidebarHeight ) && ( mainContentHeight - sidebarHeight !== 0 ) ) {
+				sidebar.addClass('is-fixed').css('bottom', 0);
+			} else {
+				sidebar.removeClass('is-fixed').attr('style', '');
+			} 
+		}
+		scrolling = false;
+	}
+
 	//on resize, move search and top nav position according to window width
 	var resizing = false;
 	moveNavigation();
 	$(window).on('resize', function(){
 		if( !resizing ) {
-			(!window.requestAnimationFrame) ? setTimeout(moveNavigation, 300) : window.requestAnimationFrame(moveNavigation);
+			if (!window.requestAnimationFrame) {
+				setTimeout(moveNavigation, 300);
+			} else { 
+				window.requestAnimationFrame(moveNavigation);
+			}
 			resizing = true;
 		}
 	});
@@ -25,7 +80,11 @@ jQuery(document).ready(function(){
 	checkScrollbarPosition();
 	$(window).on('scroll', function(){
 		if( !scrolling ) {
-			(!window.requestAnimationFrame) ? setTimeout(checkScrollbarPosition, 300) : window.requestAnimationFrame(checkScrollbarPosition);
+			if (!window.requestAnimationFrame) {
+				setTimeout(checkScrollbarPosition, 300);
+			} else {
+				window.requestAnimationFrame(checkScrollbarPosition);
+			} 
 			scrolling = true;
 		}
 	});
@@ -34,13 +93,20 @@ jQuery(document).ready(function(){
 	sidebarTrigger.on('click', function(event){
 		event.preventDefault();
 		$([sidebar, sidebarTrigger, wrapper, body]).toggleClass('nav-is-visible');
+		
+		var mq = checkMQ();        
+    if ( mq === 'mobile' ) {     	
+    	var scrollPos = $(window).scrollTop();    	
+			sidebar.offset({top: scrollPos }); //reposition sidenav on mobile to the top, just below header			
+    }
+		
 	});
 
 	//click on item and show submenu
 	$('.has-children > a').on('click', function(event){
 		var mq = checkMQ(),
 			selectedItem = $(this);
-		if( mq == 'mobile' || mq == 'tablet' ) {
+		if( mq === 'mobile' || mq === 'tablet' ) {
 			event.preventDefault();
 			if( selectedItem.parent('li').hasClass('selected')) {
 				selectedItem.parent('li').removeClass('selected');
@@ -54,9 +120,9 @@ jQuery(document).ready(function(){
 
 	//click on account and show submenu - desktop version only
 	accountInfo.children('a').on('click', function(event){
-		var mq = checkMQ(),
-			selectedItem = $(this);
-		if( mq == 'desktop') {
+		var mq = checkMQ();
+			// selectedItem = $(this);
+		if( mq === 'desktop') {
 			event.preventDefault();
 			accountInfo.toggleClass('selected');
 			sidebar.find('.has-children.selected').removeClass('selected');
@@ -85,48 +151,5 @@ jQuery(document).ready(function(){
         submenuSelector: ".has-children",
     });
 
-	function checkMQ() {
-		//check if mobile or desktop device
-		return window.getComputedStyle(document.querySelector('.cd-main-content'), '::before').getPropertyValue('content').replace(/'/g, "").replace(/"/g, "");
-	}
-
-	function moveNavigation(){
-  		var mq = checkMQ();
-        
-        if ( mq == 'mobile' && topNavigation.parents('.cd-side-nav').length == 0 ) {
-        	detachElements();
-			topNavigation.appendTo(sidebar);
-			searchForm.removeClass('is-hidden').prependTo(sidebar);
-		} else if ( ( mq == 'tablet' || mq == 'desktop') &&  topNavigation.parents('.cd-side-nav').length > 0 ) {
-			detachElements();
-			searchForm.insertAfter(header.find('.cd-logo'));
-			topNavigation.appendTo(header.find('.cd-nav'));
-		}
-		checkSelected(mq);
-		resizing = false;
-	}
-
-	function detachElements() {
-		topNavigation.detach();
-		searchForm.detach();
-	}
-
-	function checkSelected(mq) {
-		//on desktop, remove selected class from items selected on mobile/tablet version
-		if( mq == 'desktop' ) $('.has-children.selected').removeClass('selected');
-	}
-
-	function checkScrollbarPosition() {
-		var mq = checkMQ();
-		
-		if( mq != 'mobile' ) {
-			var sidebarHeight = sidebar.outerHeight(),
-				windowHeight = $(window).height(),
-				mainContentHeight = mainContent.outerHeight(),
-				scrollTop = $(window).scrollTop();
-
-			( ( scrollTop + windowHeight > sidebarHeight ) && ( mainContentHeight - sidebarHeight != 0 ) ) ? sidebar.addClass('is-fixed').css('bottom', 0) : sidebar.removeClass('is-fixed').attr('style', '');
-		}
-		scrolling = false;
-	}
+	
 });
